dd9e049227a712085e01e12c5195425aab2e3613
Thepipixia <842672164@qq.com>
2024-11-18 20:33:34 +0800

    MySQL存储引擎学习笔记

--- src/main/java/com/csj/JVMLearning/Class/Test.java
+++ src/main/java/com/csj/JVMLearning/Class/Test.java
@@ -1,25 +1,9 @@
 package com.csj.JVMLearning.Class;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Test {
 
-    static List<Object> list = new ArrayList<>();
-    static int count = 0;
-    public static void main(String[] args) throws Exception {
-        while (true) {
-            System.in.read();
-            if (count++ % 8 == 0) {
-                System.out.println("======================");
-                System.out.println("======================");
-                System.out.println("======================");
-                System.out.println("======================");
-                System.out.println("======================");
-                list.clear();
-            }
-            list.add(new byte[1024*500]);
-        }
+    public static int main(String[] args) throws Exception {
+        
     }
 }
 
